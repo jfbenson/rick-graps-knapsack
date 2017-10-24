985e61c740104ae1224a979f239051341c9aae2a
jfbenson <jfbenson@umich.edu>
2017-10-24 13:45:16 -0400

    Import player data from projections text file. Prints to screen for now.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,36 @@
+#include "player.h"
+#include "player_pool.h"
 
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 static bool is_valid_lineup(player_t *roster, size_t num_players);
 
 int main(int argc, char** argv)
 {
     // Populate player list
+    ifstream projections_file;
+
+    string line = "";
+    projections_file.open("../data/projections.txt");
+    getline(projections_file, line);
+
+    while (true)
+    {
+        getline(projections_file, line);
+        if (!projections_file)
+            break;
+
+        player_t p = player_populate(line);
+        player_print(p);
+    }
 
     return 0;
 }
 
-static bool is_valid_lineup(player_t *roster, size_t num_players);
+static bool is_valid_lineup(player_t *roster, size_t num_players)
 {
 }
--- src/player_pool.h
+++ src/player_pool.h
@@ -1,7 +1,15 @@
 #ifndef PLAYER_POOL_H
 #define PLAYER_POOL_H
 
-typedef std::unordered_map<string, player_t> player_pool_t;
+#include "player.h"
+
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+typedef unordered_map<string, player_t> player_pool_t;
+
+player_pool_t player_pool;
 
 bool player_pool_import(string filename);
 void player_pool_filter();
